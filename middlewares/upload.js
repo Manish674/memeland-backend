d619ead53818615df624406c1ce1e40b81d68b73
Manish674 <manishchoudhary4905@gmail.com>
2022-01-20 19:30:12 +0530

    finally solved the image upload problem with multer.

---
+++ middlewares/upload.js
@@ -0,0 +1,29 @@
+const multer = require("multer");
+
+// const storage = multer.diskStorage({
+//   destination(req, file, cb) {
+//     console.log("storage shit ran");
+//     cb(null, "images/");
+//   },
+//   filename(req, file, cb) {
+//     cb(null, `${file.originalname.split(".")[0]}.jpg`);
+//   },
+// });
+
+// const upload = multer({
+//   storage,
+//   limits: { fileSize: 1000000 },
+//   fileFilter(req, file, cb) {
+//     if (!file.originalname.match(/\.(jpg|jpeg|png)$/i)) {
+//       return cb(new Error("Please upload an image of type jpg jpeg png only"));
+//     }
+//     console.log("filter shit ran too");
+//     cb(undefined, true);
+//   },
+// });
+//
+const upload = multer({
+  dest: "images/",
+});
+
+exports.uploadImage = upload.single("image");
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 // const checkAuth = require("../middlewares/checkAuth");
+const { uploadImage } = require("../middlewares/upload");
 const {
   getAllPost,
   getOnePost,
@@ -9,33 +10,9 @@ const {
 } = require("../controllers/postHandler");
 
 const router = Router();
-
-const multer = require("multer");
-
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    console.log("storage shit ran");
-    cb(null, "images/");
-  },
-  filename(req, file, cb) {
-    cb(null, `${file.originalname.split(".")[0]}.jpg`);
-  },
-});
-
-const upload = multer({
-  storage,
-  limits: { fileSize: 1000000 },
-  fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(jpg|jpeg|png)$/i)) {
-      return cb(new Error("Please upload an image of type jpg jpeg png only"));
-    }
-    console.log("filter shit ran too");
-    cb(undefined, true);
-  },
-});
-
 // /api/v1/posts
-router.route("/").get(getAllPost).post(upload.single("file"), createPost);
+
+router.route("/").get(getAllPost).post(uploadImage, createPost);
 router.route("/:id").get(getOnePost).put(updatePost).delete(deletePost);
 
 module.exports = router;
