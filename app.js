b532ce75f90a9980e4fe055423f5c27b0831ac03
Manish674 <manishchoudhary4905@gmail.com>
2022-03-03 12:15:37 +0530

    cors

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const userRouter = require("./routes/userRoute");
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/posts", postRouter);
 app.use("/api/v1/user", userRouter);
