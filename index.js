61805f91109243d29ba66a672db5ce48a0f1f53e
Manish674 <manishchoudhary4905@gmail.com>
2022-03-07 21:58:06 +0530

    added | in index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const connect = require("./utils/dbConnect");
 
 connect();
 
-server.listen(process.env.PORT | 8000, () => {
+server.listen(process.env.PORT || 8000, () => {
   console.log("running");
 });
