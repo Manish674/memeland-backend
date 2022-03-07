02b5ae9dcefe5dfdac5f88ff5f54ad521d7f8ce8
Manish674 <manishchoudhary4905@gmail.com>
2022-03-07 19:20:05 +0530

    added dynamic URIs

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 // process.env.URI
 
 const connect = async () => {
-  const client = await mongoose.connect("mongodb://127.0.0.1:27017/memeland");
+  const uri =
+    process.env.NODE_ENV === "dev"
+      ? "mongodb://127.0.0.1:27017/memeland"
+      : process.env.MONGODB_URI;
+  const client = await mongoose.connect(uri);
   return client;
 };
 
