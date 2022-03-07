98e8b613bfd1a0ff3a14cb5746acf0d220034a96
Manish674 <manishchoudhary4905@gmail.com>
2022-03-07 22:08:35 +0530

    added origin

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const userRouter = require("./routes/userRoute");
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://memeland.vercel.app/");
+  next();
+});
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/posts", postRouter);
 app.use("/api/v1/user", userRouter);
