b06daf526157ec37ac95ef9d354d9e81d963ba2f
Manish Choudhary <manishchoudhary4905@gmail.com>
2023-04-11 10:07:21 +0530

    created use-case/users folder for all the use-cases realted to users

--- src/use-cases/findUser.js
+++
@@ -1,8 +0,0 @@
-const findUser = ({ userDb }) => {
-
-  return async (userInfo) => {
-    return await userDb.findOne(userInfo).exec()
-  }
-}
-
-module.exports = findUser; 
--- src/use-cases/index.js
+++
@@ -1,8 +0,0 @@
-const makeFindUser = require('./findUser');
-const makeCreateUser = require('./createUser');
-const { userDb } = require('../database/entities/');
-
-const findUser = makeFindUser({ userDb })
-const createUser = makeCreateUser({ userDb })
-
-module.exports = { findUser, createUser }
